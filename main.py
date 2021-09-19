b4616f07fca794d09e5e5ff72bf2a3a0cc964c6f
hitesh <hitesh22rana@gmail.com>
2021-09-20 03:04:05 +0530

    Added feature like to open notepad , open command prompt , open web browser , open camera and to find your own ipaddress with speech commands from user

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from sys import path
 import pyttsx3
 import speech_recognition as sr
 import datetime
 import pyaudio
 import time
+import os
+import webbrowser
+import cv2
+from requests import get
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -11,6 +17,7 @@ engine.setProperty('voices' , voices[0].id)
 # Converts text to voice
 def speak(audio):
     engine.say(audio)
+    print(audio)
     engine.runAndWait()
 
 # Converts voice to text and even speaks
@@ -34,6 +41,7 @@ def take_command():
     
     return query
 
+# Wish Function
 def wish():
     hour = int(datetime.datetime.now().hour)
 
@@ -46,7 +54,57 @@ def wish():
     else:
         speak("Good Evening Sir!")
 
+    speak("How Can i Help you!")
 
 
 if __name__ == "__main__":
-    wish()
+    # Wish function
+    # wish()
+
+    while True:
+        query = take_command().lower()
+
+        #Logic for tasks
+
+        # feature to open notepad
+
+        if ("open notepad") in query:
+            path = "C:\\Windows\\system32\\notepad.exe"
+            os.startfile(path)
+
+
+        # feature to open cmd
+
+        elif ("open command prompt") in query:
+            os.system("start cmd")
+
+    
+        # feature to open web-browser
+
+        elif ("open web browser") in query:
+            # speak("What do you want me to search!")
+            # search = take_command().lower()
+            webbrowser.open(f"https://www.google.co.in")
+
+
+        # feature to open camera
+
+        elif ("open camera") in query:
+            speak("Opening Camera!")
+
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret , img = cap.read()
+                cv2.imshow('webcam', img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+
+
+        # feature to get own ipaddress
+
+        elif ("ip address") in query:
+            ip = get('https://api.ipify.org').text
+            speak(f"Your IP Address is {ip}")      
+
